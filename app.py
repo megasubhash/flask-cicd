da60df72ae1e5796839e56fcfc9462fc006e8ccf
Subhash Sahu <subhash.sahu@ingrammicro.com>
2020-02-18 19:38:53 +0530

    added

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS, cross_origin
+import requests
+
+from datetime import datetime
+
+
+
+
+
+app = Flask(__name__,static_url_path='', 
+            static_folder='web/static',
+            template_folder='templates')
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route('/getpage', methods=['GET', 'POST'])
+@cross_origin()
+def root():
+    return render_template("page.html",message="hello from Flask")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80,debug=True)
